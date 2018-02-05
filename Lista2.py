3a6e5f183a0ea55d57333b23ba499d540f6b217f
Ana Paula Fernandes de Souza <paula253@hotmail.com>
2018-02-05 19:16:58 -0200

    Lista 2 exercicio 1 - 8 finalizados.

---
+++ Lista2.py
@@ -0,0 +1,80 @@
+#-------------------------------------------------------
+#Nome : Ana Paula Fernandes de Souza
+#Atualização : 05/02/2018
+#Lista 2
+#Assunto : Tomada de Decisão
+#-------------------------------------------------------
+
+
+# 1) Faça um algoritmo que leia um número e mostre se o mesmo é positivo, negativo ou zero.
+
+
+# num = float(input("Digite um numero"))
+#
+# if(num==0):
+#     print("O numero é zero")
+# elif(num<0):
+#     print("O numero é negativo")
+# else:
+#     print("O numero é positivo")
+
+
+# 2 - 7) Faça um algoritmo que leia um número e mostre se o mesmo é par ou ímpar.
+
+# num = int(input("Digite um numero "))
+#
+# tipo= "par" if(num%2==0)  else "impar"
+#
+# print("O numero é ",tipo)
+
+
+# 3) Faça um algoritmo que leia dois números e imprima o maior.
+
+# num1 = float(input("Digite o primeiro numero"))
+# num2 = float(input("Digite o segundo numero"))
+#
+# if(num1>num2):
+#     print("O maior numero é ",num1)
+# else:
+#     print("O maior numero é ",num2)
+
+# 4) Faça um algoritmo que peça a idade de uma pessoa e
+# imprima na tela se a mesma já é maior de idade ou então, se a mesma é de menor.
+
+
+# idade=int(input("Digite sua idade "))
+#
+# if(idade<18):
+#     print("Você é de menor")
+# else:
+#     print("Você é de maior")
+
+# 5) Faça um algoritmo que peça a idade do usuário e a idade da sua mãe.
+# Em seguida, imprima na tela com quantos anos sua mãe o teve.
+
+# idade_mae=int(input("Digite a idade da sua mae : "))
+# idade=int(input("Digite a sua idade : "))
+#
+# print("Sua mae te teve com ",idade_mae-idade)
+
+# 6) Faça um algoritmo que imprima 50 vezes o caractere "-" na tela (sem a utilização de laços de repetição).
+
+# print("-"*50)
+
+
+# 8) Faça um algoritmo que peça dois números. Primeiro, imprima o maior e, em seguida, o menor.
+
+
+# num1 = float(input("Digite o primeiro numero"))
+# num2 = float(input("Digite o segundo numero"))
+
+# if(num1>num2):
+#     print("O maior numero é ",num1)
+#     print("O menor numero é ",num2)
+# else:
+#     print("O maior numero é ",num2)
+#     print("O menor numero é ",num1)
+
+
+# 9) Faça um algoritmo que verifica se um determinado valor é um número inteiro.
+
